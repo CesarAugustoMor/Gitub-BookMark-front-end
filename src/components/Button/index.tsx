1fc66998e8aa64cbb92b782a65729cbf27729373
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2021-03-31 18:31:43 -0300

    fix in component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,32 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons/lib';
+
 import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   loading?: boolean;
+  textLoading?: string;
+  icon?: React.ComponentType<IconBaseProps>;
+  sizeIcon?: number;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
+const Button: React.FC<ButtonProps> = ({
+  children,
+  loading,
+  textLoading = 'Loading...',
+  icon: Icon,
+  sizeIcon = 32,
+  ...rest
+}) => (
   <Container type="button" {...rest}>
-    {loading ? 'Carregando...' : children}
+    {loading ? (
+      textLoading
+    ) : (
+      <>
+        {Icon && <Icon size={sizeIcon} />}
+        {children}
+      </>
+    )}
   </Container>
 );
 
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,18 +2,31 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Container = styled.button`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 10px 16px;
+  border-radius: 10px;
+  margin-top: 16px;
+
   background: ${prps => prps.theme.colors.primary};
   height: 56px;
-  border-radius: 10px;
   border: 0;
-  padding: 0 16px;
   color: ${props => props.theme.colors.text};
   width: 100%;
   font-weight: 500;
-  margin-top: 16px;
   transition: background-color 0.2s;
 
   &:hover {
     background: ${props => shade(0.2, props.theme.colors.primary)};
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+
+  span {
+    margin-top: 0 !important;
   }
 `;
