382f189320630dc9a8ea0934a8b4fcddc978ec2a
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2021-03-31 17:35:56 -0300

    page not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-const Main: React.FC = () => {
-  return (
-    <Redirect
-      to={{
-        pathname: '/',
-      }}
-    />
-  );
-};
+const Main: React.FC = () => (
+  <Redirect
+    to={{
+      pathname: '/',
+    }}
+  />
+);
 
 export default Main;
