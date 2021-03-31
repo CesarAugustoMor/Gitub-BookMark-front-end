1b3bb232084c683a16c227349f8dc1425b8caa63
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2021-03-31 17:41:48 -0300

    page SigIn

---
+++ src/pages/SingIn/index.tsx
@@ -0,0 +1,102 @@
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import React, { useCallback, useRef } from 'react';
+import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import * as Yup from 'yup';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logo from '../../assets/logo.svg';
+
+import { AnimatedContainer, Container } from './styles';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handlesubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória.'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        history.push('/dashboard');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+
+          formRef.current?.setErrors(errors);
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer logIn, cheque as credenciais',
+        });
+      }
+    },
+    [addToast, history, signIn],
+  );
+
+  return (
+    <Container>
+      <AnimatedContainer>
+        <img src={logo} alt="GitHub BookMark" />
+
+        <Form ref={formRef} onSubmit={handlesubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          {/* <Link to="forgot_password">Esqueci minha senha</Link> */}
+        </Form>
+
+        <Link to="/signup">
+          <FiLogIn />
+          Criar conta
+        </Link>
+      </AnimatedContainer>
+    </Container>
+  );
+};
+
+export default SignIn;
